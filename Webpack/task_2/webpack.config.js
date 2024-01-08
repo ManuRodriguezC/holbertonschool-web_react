d0d4f05b16bf101d5fe7003ff9114152e2772b92
ManuRodriguezC <estebanclimb@gmail.com>
2024-01-07 22:18:45 -0500

    Add css and images

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -9,16 +9,41 @@ module.exports = {
     },
     module: {
         rules: [
-          {
-            test: /\.(jpg|png)$/,
-            use: {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: 'images/'
-              }
-            }
-          },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'images/'
+                    }
+                  },
+                  {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      disable: false,
+                      mozjpeg: {
+                        progressive: true,
+                        quality: 65
+                      },
+                      optipng: {
+                        enabled: false,
+                      },
+                      pngquant: {
+                        quality: [0.65, 0.90],
+                        speed: 4
+                      },
+                      gifsicle: {
+                        interlaced: false,
+                      },
+                      webp: {
+                        quality: 75
+                      }
+                    }
+                  }
+                ]
+              },
           {
             test: /\.css$/,
             use: ['style-loader', 'css-loader']
