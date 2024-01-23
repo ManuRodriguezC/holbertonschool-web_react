55e2210fed7e52aee4261584cc1ecdc62815d395
ManuRodriguezC <estebanclimb@gmail.com>
2024-01-23 10:41:28 -0500

    Reorganize the files

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -9,8 +9,9 @@ module.exports = {
     ],
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '../dist'),
     },
+    mode: 'development',
     devtool: 'inline-source-map',
     module: {
         rules: [
@@ -19,17 +20,22 @@ module.exports = {
                 use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
+                test: /\.(png|jpe?g|gif)$/,
                 use: [
                     {
                         loader: 'image-webpack-loader',
                     },
                 ],
             },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            }
         ],
     },
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: path.join(__dirname, '../dist'),
         compress: true,
         port: 9000,
         hot: true,
