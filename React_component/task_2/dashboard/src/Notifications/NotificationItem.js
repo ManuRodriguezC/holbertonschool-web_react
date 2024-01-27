b19f337e7b7834f5d0983bb3b45b7c3f0d578ed1
ManuRodriguezC <estebanclimb@gmail.com>
2024-01-27 11:46:48 -0500

    Implement use meme, aun HOC in some components

---
+++ React_component/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Notifications.css'
+
+export default function NotificationItem({ id ,type, html, value, markAsRead = () => {} }) {
+    return (
+        <li data-notification-type={type} onClick={() => markAsRead(id)}>
+            <span dangerouslySetInnerHTML={{ __html: html + value }} />
+        </li>
+    );
+};
+
+NotificationItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  type: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
+  html: PropTypes.shape({
+      __html: PropTypes.string.isRequired,
+  }).isRequired,
+  value: PropTypes.string.isRequired,
+  markAsRead: PropTypes.func.isRequired,
+};
+
+NotificationItem.defaultProps = {
+    type: 'default',
+};
---
+++ React_component/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './Notifications.css'
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types'
+
+class Notifications extends React.Component {
+
+    static propTypes = {
+        displayDrawer: PropTypes.bool
+    }
+
+    static defaultProps = {
+        displayDrawer: false
+    }
+
+    markAsRead = (id) => {
+        console.log(`Notification ${id} has been marked as read`);
+    }
+
+    render() {
+        const { displayDrawer, listNotifications = [] } = this.props;
+
+        return (
+            <div className='menuItem'>
+                <p>Your notifications</p>
+                {displayDrawer && (
+                    <div className='Notifications'>
+                        <button
+                            style={{float: 'right', display: 'inline'}}
+                            aria-label='Close'
+                            onClick={() => {console.log('Close button has been clicked')}}
+                            >x
+                        </button>
+                        <p>Here is the list of notifications</p>
+                        <ul>
+                            {listNotifications.length > 0 ? listNotifications.map((noti, index) => {
+                                return (
+                                    <NotificationItem 
+                                        key={noti.id}
+                                        id={index}
+                                        type={noti.type}
+                                        html={noti.html}
+                                        value={noti.value}
+                                        markAsRead={this.markAsRead}
+                                    />
+                                )
+                            }) : (
+                                < NotificationItem key="no-new-notification" html={""} value='No new notification for now'/>
+                            )}
+                        </ul>
+                    </div>
+                    )
+                }
+            </div>
+        )
+    }
+}
+
+export default Notifications;
+
+
+Notification.propTypes = {
+    displayDrawer: PropTypes.bool
+}
+
+Notification.defaultProps = {
+    displayDrawer: false
+}
